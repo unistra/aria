b537581e1043d6ace4b26ac9d8dcffd2975d1dac
Christophe Boccheciampe <cb at unistra.fr>
2014-12-11 09:03:03 +0100

    Fix encoding

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 APPLICATION ARIA - UNIVERSITE DE STRASBOURG
 
 LICENCE : CECILL-B
-Copyright UniversitÃ© de Strasbourg
+Copyright Université de Strasbourg
 Contributeur : Christophe Boccheciampe - Janvier 2006
 Adresse : cb@dpt-info.u-strasbg.fr
 
-L'application utilise des Ã©lÃ©ments Ã©crits par des tiers, placÃ©s sous les licences suivantes :
+L'application utilise des éléments écrits par des tiers, placés sous les licences suivantes :
 
-IcÃ´nes :
+Icônes :
 - CrystalSVG (http://www.everaldo.com), sous licence LGPL (http://www.gnu.org/licenses/lgpl.html).
 - Oxygen (http://oxygen-icons.org) sous licence LGPL-V3
 - KDE (http://www.kde.org) sous licence LGPL-V2
@@ -20,30 +20,30 @@ Librairie FPDF : http://fpdf.org (licence permissive sans restriction d'usage)
 =======================================================================================================
 [CECILL-B]
 
-Ce logiciel est un programme informatique permettant Ã  des candidats de dÃ©poser un ou plusieurs
-dossiers de candidatures dans une universitÃ©, et aux gestionnaires de cette derniÃ¨re de traiter ces
+Ce logiciel est un programme informatique permettant à des candidats de déposer un ou plusieurs
+dossiers de candidatures dans une université, et aux gestionnaires de cette dernière de traiter ces
 demandes.
 
-Ce logiciel est rÃ©gi par la licence CeCILL-B soumise au droit franÃ§ais et respectant les principes de
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et respectant les principes de
 diffusion des logiciels libres. Vous pouvez utiliser, modifier et/ou redistribuer ce programme sous les
-conditions de la licence CeCILL-B telle que diffusÃ©e par le CEA, le CNRS et l'INRIA sur le site
+conditions de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA sur le site
 "http://www.cecill.info".
 
-En contrepartie de l'accessibilitÃ© au code source et des droits de copie, de modification et de
-redistribution accordÃ©s par cette licence, il n'est offert aux utilisateurs qu'une garantie limitÃ©e.
-Pour les mÃªmes raisons, seule une responsabilitÃ© restreinte pÃ¨se sur l'auteur du programme, le titulaire
-des droits patrimoniaux et les concÃ©dants successifs.
+En contrepartie de l'accessibilité au code source et des droits de copie, de modification et de
+redistribution accordés par cette licence, il n'est offert aux utilisateurs qu'une garantie limitée.
+Pour les mêmes raisons, seule une responsabilité restreinte pèse sur l'auteur du programme, le titulaire
+des droits patrimoniaux et les concédants successifs.
 
-A cet Ã©gard l'attention de l'utilisateur est attirÃ©e sur les risques associÃ©s au chargement, Ã 
-l'utilisation, Ã  la modification et/ou au dÃ©veloppement et Ã  la reproduction du logiciel par l'utilisateur
-Ã©tant donnÃ© sa spÃ©cificitÃ© de logiciel libre, qui peut le rendre complexe Ã  manipuler et qui le rÃ©serve
-donc Ã  des dÃ©veloppeurs et des professionnels avertis possÃ©dant  des  connaissances informatiques
-approfondies. Les utilisateurs sont donc invitÃ©s Ã  charger et tester l'adÃ©quation du logiciel Ã  leurs
-besoins dans des conditions permettant d'assurer la sÃ©curitÃ© de leurs systÃ¨mes et ou de leurs donnÃ©es et,
-plus gÃ©nÃ©ralement, Ã  l'utiliser et l'exploiter dans les mÃªmes conditions de sÃ©curitÃ©.
+A cet égard l'attention de l'utilisateur est attirée sur les risques associés au chargement, à
+l'utilisation, à la modification et/ou au développement et à la reproduction du logiciel par l'utilisateur
+étant donné sa spécificité de logiciel libre, qui peut le rendre complexe à manipuler et qui le réserve
+donc à des développeurs et des professionnels avertis possédant  des  connaissances informatiques
+approfondies. Les utilisateurs sont donc invités à charger et tester l'adéquation du logiciel à leurs
+besoins dans des conditions permettant d'assurer la sécurité de leurs systèmes et ou de leurs données et,
+plus généralement, à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accÃ©der Ã  cet en-tÃªte signifie que vous avez pris connaissance de la licence
-CeCILL-B, et que vous en avez acceptÃ© les termes.
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez pris connaissance de la licence
+CeCILL-B, et que vous en avez accepté les termes.
 
 =======================================================================================================
 */
@@ -73,7 +73,7 @@ CeCILL-B, et que vous en avez acceptÃ© les termes.
    else $not_found=1;
 
    if(isset($not_found) && $not_found==1)
-      die("Configuration de l'interface incomplÃ¨te - AccÃ¨s impossible.\n");
+      die("Configuration de l'interface incomplète - Accès impossible.\n");
 
    $dbr=db_connect();
 
@@ -82,7 +82,7 @@ CeCILL-B, et que vous en avez acceptÃ© les termes.
 
    if($load_config==FALSE) // config absente : erreur
       $erreur_config=1;
-   elseif($load_config==-1) // paramÃ¨tre(s) manquant(s) : avertissement
+   elseif($load_config==-1) // paramètre(s) manquant(s) : avertissement
       $warn_config=1;
 
    $php_self=$_SERVER['PHP_SELF'];
@@ -101,7 +101,7 @@ CeCILL-B, et que vous en avez acceptÃ© les termes.
    $_SESSION["auth"]=0;
    $_SESSION["interface_ouverte"]=0;
 
-   // SÃ©lection des dates limites dans les formations (au cas oÃ¹ l'une d'elles serait postÃ©rieure Ã  la date de fermeture ou antÃ©rieure Ã  celle d'ouverture)
+   // Sélection des dates limites dans les formations (au cas où l'une d'elles serait postérieure à la date de fermeture ou antérieure à celle d'ouverture)
    $result=db_query($dbr,"SELECT min($_DBC_session_ouverture), max($_DBC_session_fermeture) FROM $_DB_session");
    $rows=db_num_rows($result);
 
@@ -124,25 +124,25 @@ CeCILL-B, et que vous en avez acceptÃ© les termes.
 
 <div class='main'>
    <?php
-      titre_page_icone("Bienvenue sur l'interface de PrÃ©candidatures en ligne", "", 15, "C");
+      titre_page_icone("Bienvenue sur l'interface de Précandidatures en ligne", "", 15, "C");
    ?>
 
    <div style='width:80%; text-align:justify; margin-left:auto; margin-right:auto;'>
       <p class='Texte'>
-         Cette interface permet d'accÃ©lÃ©rer le traitement de vos voeux en nous transmettant directement vos donnÃ©es. Seuls les justificatifs de
-         vos diplÃ´mes et autres piÃ¨ces administratives vous seront demandÃ©es par voie postale.
+         Cette interface permet d'accélérer le traitement de vos voeux en nous transmettant directement vos données. Seuls les justificatifs de
+         vos diplômes et autres pièces administratives vous seront demandées par voie postale.
       </p>
 
       <ul>
          <li style='padding-bottom:5px;'><a href='<?php echo "$__DOC_DIR/documentation.php"; ?>' target='_blank' class='lien_bleu_14'><b>Cliquez ici pour consulter la documentation en ligne</b></a></li>
-         <li style='padding-bottom:5px;'><a href='<?php echo "$__DOC_DIR/limites.php"; ?>' target='_self' class='lien_bleu_14'><b>Cliquez ici pour vÃ©rifier les dates limites de candidature Ã  nos formations</b></a></li>
+         <li style='padding-bottom:5px;'><a href='<?php echo "$__DOC_DIR/limites.php"; ?>' target='_self' class='lien_bleu_14'><b>Cliquez ici pour vérifier les dates limites de candidature à nos formations</b></a></li>
          <li style='padding-bottom:5px;'><a href='<?php echo "$__DOC_DIR/conditions.php"; ?>' target='_blank' class='lien_rouge_14'><b>IMPORTANT : Cliquez ici pour lire les conditions d'utilisation de l'interface</b></a></li>
       </ul>
    
       <table width='100%' cellpadding="0" cellspacing="0" border='0'>
       <tr>
          <td valign="left" colspan='2' style="background : url(<?php echo "$__IMG_DIR/fond_gradient_menu2.png"; ?>) no-repeat; padding:4px 0px 4px 4px; vertical-align:middle;">
-            <font class='Texte3'><u><b>TÃ©lÃ©chargements gratuits :</b></u></font>
+            <font class='Texte3'><u><b>Téléchargements gratuits :</b></u></font>
          </td>
       </tr>
       <tr>
@@ -184,7 +184,7 @@ CeCILL-B, et que vous en avez acceptÃ© les termes.
 
       
       <font class='Texte_10'>
-         <strong><u>Informatique et LibertÃ©s</u></strong>
+         <strong><u>Informatique et Libertés</u></strong>
          <br>
          <div style='text-align:justify; padding-bottom:20px;'>
             <?php
