28972db45a856a17e4d49fb3799fd52c6cb649af
Christophe Boccheciampe <cb at unistra.fr>
2015-04-09 10:23:45 +0200

    cosmetic only

--- interface/menu_identite.php
+++ interface/menu_identite.php
@@ -49,172 +49,172 @@ CeCILL-B, et que vous en avez accepté les termes.
 */
 ?>
 <?php
-	// Vérifications complémentaires au cas où ce fichier serait appelé directement
-	if(!isset($_SESSION["authentifie"]))
-	{
-		session_write_close();
-		header("Location:../index.php");
-		exit();
-	}
+  // Vérifications complémentaires au cas où ce fichier serait appelé directement
+  if(!isset($_SESSION["authentifie"]))
+  {
+    session_write_close();
+    header("Location:../index.php");
+    exit();
+  }
 
-	if(!isset($_SESSION["comp_id"]) || (isset($_SESSION["comp_id"]) && $_SESSION["comp_id"]==""))
-	{
-		session_write_close();
-		header("Location:composantes.php");
-		exit();
-	}
+  if(!isset($_SESSION["comp_id"]) || (isset($_SESSION["comp_id"]) && $_SESSION["comp_id"]==""))
+  {
+    session_write_close();
+    header("Location:composantes.php");
+    exit();
+  }
 
-	print("<div class='centered_box' style='padding-bottom:10px;'>
-				<font class='Texte_16'><strong>$_SESSION[onglet] - Vous</strong></font>
-			 </div>\n");
+  print("<div class='centered_box' style='padding-bottom:10px;'>
+        <font class='Texte_16'><strong>$_SESSION[onglet] - Vous</strong></font>
+       </div>\n");
 
-	// La valeur de $__lock est déterminée dans le script precandidatures.php (qui inclut tous les menu_*.php)
-	if($__lock)		
-		message("<center>
-						Certaines formations sélectionnées dans cette composante sont verrouillées.
-						<br>Merci d'envoyer vos justificatifs le plus rapidement possible !
-					</center>", $__WARNING);
+  // La valeur de $__lock est déterminée dans le script precandidatures.php (qui inclut tous les menu_*.php)
+  if($__lock)   
+    message("<center>
+            Certaines formations sélectionnées dans cette composante sont verrouillées.
+            <br>Merci d'envoyer vos justificatifs le plus rapidement possible !
+          </center>", $__WARNING);
 
-	if(isset($_GET["sed"]) && $_GET["sed"]==1)
-		message("Informations modifiées avec succès.", $__SUCCES);
+  if(isset($_GET["sed"]) && $_GET["sed"]==1)
+    message("Informations modifiées avec succès.", $__SUCCES);
 
-	print("<div class='centered_box' style='padding-bottom:10px;'>
-				<a href='edit_candidat.php' target='_self' class='lien2'><img class='icone' src='$__ICON_DIR/edit_22x22_fond.png' border='0' alt='Modifier' desc='Modifier'></a>
-				<a href='edit_candidat.php' target='_self' class='lien2'>Modifier ces informations</a>
-			</div>\n");
+  print("<div class='centered_box' style='padding-bottom:10px;'>
+        <a href='edit_candidat.php' target='_self' class='lien2'><img class='icone' src='$__ICON_DIR/edit_22x22_fond.png' border='0' alt='Modifier' desc='Modifier'></a>
+        <a href='edit_candidat.php' target='_self' class='lien2'>Modifier ces informations</a>
+      </div>\n");
 ?>
 
 <table align='center'>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Vous : </b></font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'>
-		   <strong>
-		   <?php 
-		      print("$civ_texte $_SESSION[nom] $_SESSION[prenom]"); 
-		      
-		      if($_SESSION["prenom2"]!="")
-		         echo " (".$_SESSION["prenom2"].")";
-			?>
-			</strong>
-		</font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Vous : </b></font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'>
+       <strong>
+       <?php 
+          print("$civ_texte $_SESSION[nom] $_SESSION[prenom]"); 
+          
+          if($_SESSION["prenom2"]!="")
+             echo " (".$_SESSION["prenom2"].")";
+      ?>
+      </strong>
+    </font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Nom de naissance</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[nom_naissance]"); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Nom de naissance</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$_SESSION[nom_naissance]"); ?></font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Numéro INE</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[numero_ine]"); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Numéro INE</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$_SESSION[numero_ine]"); ?></font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b><?php echo $ne_le; ?> : </b></font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$txt_naissance à $_SESSION[lieu_naissance] - ".stripslashes($_SESSION["pays_naissance"])); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b><?php echo $ne_le; ?> : </b></font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$txt_naissance à $_SESSION[lieu_naissance] - ".stripslashes($_SESSION["pays_naissance"])); ?></font>
+  </td>
 </tr>
 <?php
-	if(isset($_SESSION["dpt_naissance"]) && $_SESSION["dpt_naissance"]!="" && isset($_SESSION["nom_departement"]) && $_SESSION["nom_departement"]!="")
-	{
+  if(isset($_SESSION["dpt_naissance"]) && $_SESSION["dpt_naissance"]!="" && isset($_SESSION["nom_departement"]) && $_SESSION["nom_departement"]!="")
+  {
 ?>
 <tr>
    <td nowrap='true' class='td-gauche fond_menu2'>
-   	<font class='Texte_menu2'><b>Département de naissance : </b></font>
-	</td>
+    <font class='Texte_menu2'><b>Département de naissance : </b></font>
+  </td>
    <td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[dpt_naissance] - $_SESSION[nom_departement]"); ?></font>
-	</td>
+    <font class='Texte_menu'><?php print("$_SESSION[dpt_naissance] - $_SESSION[nom_departement]"); ?></font>
+  </td>
 </tr>
-<?php	
-	}
+<?php 
+  }
 ?>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Nationalité</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php echo preg_replace("/_/","",$_SESSION['nationalite']); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Nationalité</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php echo preg_replace("/_/","",$_SESSION['nationalite']); ?></font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Téléphone</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[telephone]"); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Téléphone</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$_SESSION[telephone]"); ?></font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Téléphone portable</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[telephone_portable]"); ?></font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Téléphone portable</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$_SESSION[telephone_portable]"); ?></font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Adresse</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php echo html_entity_decode(stripslashes($adresse)); ?>&nbsp;</font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Adresse</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php echo html_entity_decode(stripslashes($adresse), ENT_COMPAT | ENT_HTML401, "UTF-8"); ?>&nbsp;</font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Adresse électronique (<i>email</i>)</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'><?php print("$_SESSION[email]"); ?>&nbsp;</font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Adresse électronique (<i>email</i>)</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'><?php print("$_SESSION[email]"); ?>&nbsp;</font>
+  </td>
 </tr>
 <tr>
-	   <td class='fond_page' colspan='2' height='10'></td>
-	</tr>
+     <td class='fond_page' colspan='2' height='10'></td>
+  </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'><b>Baccalauréat (ou équivalent)</b> : </font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'>
-			<?php  
-				print("$_SESSION[annee_bac] - $_SESSION[nom_serie_bac]");
-			?>
-		</font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'><b>Baccalauréat (ou équivalent)</b> : </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'>
+      <?php  
+        print("$_SESSION[annee_bac] - $_SESSION[nom_serie_bac]");
+      ?>
+    </font>
+  </td>
 </tr>
 <tr>
-	<td nowrap='true' class='td-gauche fond_menu2'>
-		<font class='Texte_menu2'>
-		   <strong>
-		      Inscription antérieure
-		      <br />dans cette Université ? :
-			</strong>
-		</font>
-	</td>
-	<td nowrap='true' class='td-droite fond_menu'>
-		<font class='Texte_menu'>
-			<?php  
-			   if($_SESSION["deja_inscrit"]==1)
-			      print("Oui (".$_SESSION["annee_premiere_inscr"].")");
-				else
-				   print("Non");
-			?>
-		</font>
-	</td>
+  <td nowrap='true' class='td-gauche fond_menu2'>
+    <font class='Texte_menu2'>
+       <strong>
+          Inscription antérieure
+          <br />dans cette Université ? :
+      </strong>
+    </font>
+  </td>
+  <td nowrap='true' class='td-droite fond_menu'>
+    <font class='Texte_menu'>
+      <?php  
+         if($_SESSION["deja_inscrit"]==1)
+            print("Oui (".$_SESSION["annee_premiere_inscr"].")");
+        else
+           print("Non");
+      ?>
+    </font>
+  </td>
 </tr>
 </table>
 
